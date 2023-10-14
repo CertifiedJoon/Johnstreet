9900b699aa97c802f1db40605dded6e5fae4b20d
Joon_Thinkpad <mjypark1212@gmail.com>
2023-10-14 14:49:04 +0800

    wip

--- bond_buyer.py
+++ bond_buyer.py
@@ -27,6 +27,8 @@ class BondBuyer:
         buy_id = self._oid[sym + "B"]
         sell_id = self._oid[sym + "S"]
 
+        print(f"bond order at " + buy_price)
+
         self._exchange.send_add_message(
             order_id=buy_id, symbol=sym, dir=Dir.BUY, price=buy_price, size=1
         )
