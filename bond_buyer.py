917385cb323b9ed09eee82ec95d310b2b47b0335
Joon_Thinkpad <mjypark1212@gmail.com>
2023-10-14 14:43:58 +0800

    wip

---
+++ bond_buyer.py
@@ -0,0 +1,35 @@
+from enum import Enum
+from jane_street import ExchangeConnection as Exchange
+from jane_street import Dir
+
+
+class BondBuyer:
+    def __init__(self, exchange, delta):
+        self._exchange = exchange
+        self._base_id = 1_000_000
+        self._delta = delta
+        self._assets = ["BOND"]
+        self._oid = {}
+        for asset in self._assets:
+            self._oid[asset + "B"] = self._base_id + 1
+            self._oid[asset + "S"] = self._base_id + 2
+            self._base_id += 2
+
+    def listen(self, msg):
+        if msg["type"] != "trade" or msg["symbol"] not in self._assets:
+            return
+
+        sym = msg["symbol"]
+
+        buy_price = 1000 - self._delta
+        sell_price = 100 + self._delta
+
+        buy_id = self._oid[sym + "B"]
+        sell_id = self._oid[sym + "S"]
+
+        self._exchange.send_add_message(
+            order_id=buy_id, symbol=sym, dir=Dir.BUY, price=buy_price, size=1
+        )
+        self._exchange.send_add_message(
+            order_id=sell_id, symbol=sym, dir=Dir.SELL, price=sell_price, size=1
+        )
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from jane_street import parse_arguments
 from supporter import Supporter
 from arbitrage import Arbitrage
 import multiprocessing
+from bond_buyer import BondBuyer
 
 # ~~~~~============== CONFIGURATION  ==============~~~~~
 team_name = "JohnStreet"
@@ -45,6 +46,18 @@ def arbitrage_loop(exchange: Exchange):
             break
 
 
+def bond_loop(exchange: Exchange, delta):
+    bb = BondBuyer(exchange, delta)
+
+    while True:
+        message = exchange.read_message()
+        bb.listen(message)
+
+        if message["type"] == "close":
+            print("the round has ended, market maker stop.")
+            break
+
+
 def main():
     args = parse_arguments()
 
@@ -53,24 +66,24 @@ def main():
     mm_thread_cnt = 15
     mm_loops = []
 
-    # for i in range(1, mm_thread_cnt):
-    #     mm_loops.append(
-    #         multiprocessing.Process(target=market_maker_loop, args=(exchange, i))
-    #     )
+    for i in range(1, mm_thread_cnt):
+        mm_loops.append(
+            multiprocessing.Process(target=market_maker_loop, args=(exchange, i))
+        )
 
-    # ml_loop = multiprocessing.Process(target=market_logger_loop, args=(exchange,))
+    ml_loop = multiprocessing.Process(target=market_logger_loop, args=(exchange,))
     arb_loop = multiprocessing.Process(target=arbitrage_loop, args=(exchange,))
 
     # # starting process 1
-    # for i in range(1, mm_thread_cnt):
-    #     mm_loops[i - 1].start()
-    # ml_loop.start()
+    for i in range(1, mm_thread_cnt):
+        mm_loops[i - 1].start()
+    ml_loop.start()
     arb_loop.start()
 
     # wait until process 1 is finished
-    # for i in range(1, mm_thread_cnt):
-    #     mm_loops[i - 1].join()
-    # ml_loop.join()
+    for i in range(1, mm_thread_cnt):
+        mm_loops[i - 1].join()
+    ml_loop.join()
     arb_loop.join()
 
     # both processes finished
